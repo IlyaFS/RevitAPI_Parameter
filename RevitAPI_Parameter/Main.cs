edc7b7f04a2b543c055d49743854f3f1e50429f1
Илья Филиппов <filippov.i.s@gmail.com>
2022-05-01 13:36:56 +0300

    Добавьте файлы проекта.

---
+++ RevitAPI_Parameter/Main.cs
@@ -0,0 +1,94 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI_Parameter
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+
+            var categorySet = new CategorySet();
+            categorySet.Insert(Category.GetCategory(doc, BuiltInCategory.OST_PipeCurves));
+
+            using (Transaction ts = new Transaction(doc, "Add parameter"))
+            {
+                ts.Start();
+                CreateSharadParameter(uiapp.Application, doc, "Наименование", categorySet, BuiltInParameterGroup.PG_IDENTITY_DATA, true);
+                ts.Commit();
+            }
+
+            var pipes = new FilteredElementCollector(doc)
+                .OfClass(typeof(Pipe))
+                .Cast<Pipe>()
+                .ToList();
+
+            foreach (var pipe in pipes)
+            {
+                double externalDiameterValue = 0;
+                double innerDiameterValue = 0;
+                Parameter externalDiameter = pipe.get_Parameter(BuiltInParameter.RBS_PIPE_OUTER_DIAMETER);
+                if (externalDiameter.StorageType == StorageType.Double)
+                {
+                    externalDiameterValue = UnitUtils.ConvertFromInternalUnits(externalDiameter.AsDouble(), UnitTypeId.Millimeters);
+                }
+                Parameter innerDiameter = pipe.get_Parameter(BuiltInParameter.RBS_PIPE_INNER_DIAM_PARAM);
+                if (innerDiameter.StorageType == StorageType.Double)
+                {
+                    innerDiameterValue = UnitUtils.ConvertFromInternalUnits(innerDiameter.AsDouble(), UnitTypeId.Millimeters);
+                }
+                using (Transaction ts = new Transaction(doc, "Set parameters"))
+                {
+                    ts.Start();
+                    Parameter name = pipe.LookupParameter("Наименование");
+                    name.Set($"Труба {externalDiameterValue.ToString()}/{innerDiameterValue.ToString()}");
+                    ts.Commit();
+                }
+            }
+
+            return Result.Succeeded;
+        }
+
+        private void CreateSharadParameter(Application application,
+            Document doc, string parameterName, CategorySet categorySet,
+            BuiltInParameterGroup builtInParameterGroup, bool isInstance)
+        {
+            DefinitionFile definitionFile = application.OpenSharedParameterFile();
+            if (definitionFile == null)
+            {
+                TaskDialog.Show("Ошибка", "Не найден файл общих параметров");
+                return;
+            }
+
+            Definition definition = definitionFile.Groups
+                .SelectMany(group => group.Definitions)
+                .FirstOrDefault(def => def.Name.Equals(parameterName));
+            if (definition == null)
+            {
+                TaskDialog.Show("Ошибка", "Не найден указанный параметр");
+                return;
+            }
+
+            Binding binding = application.Create.NewTypeBinding(categorySet);
+            if (isInstance)
+                binding = application.Create.NewInstanceBinding(categorySet);
+
+            BindingMap map = doc.ParameterBindings;
+            map.Insert(definition, binding, builtInParameterGroup);
+        }
+    }
+}
